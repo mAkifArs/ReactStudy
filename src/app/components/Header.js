b110e6992e8b6e576465ced5d7b77a605986b673
mehmetakifarslan <grottulf5@gmail.com>
2021-07-04 18:20:50 +0300

    how props work?

---
+++ src/app/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Header =  (props) =>  {
+    return(
+        <nav className={"navbar navbar-default"}>
+            <div className={"container"}>
+                <ul className="nav navbar-nav">
+                    <li><a href="#">{props.homeLink}</a></li>
+                </ul>
+            </div>
+        </nav>
+)
+}
--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -3,28 +3,50 @@ import PropTypes from 'prop-types';
 
 
 export class Home extends React.Component{
+    constructor(props) {
+        super();
+        this.state = {
+            age: props.initialAge,
+            status: 0,
+            homeLink: props.initialLinkName
+        };
+        setTimeout(() =>{this.setState({status:1})} ,3000)
+    }
+    onMakeOlder(){
+        this.setState({
+            age: this.state.age + 3
+        });
+    }
+    onChangeLink(){
+        this.props.changeLink(this.state.homeLink);
+    }
+    onHandleChange(event){
+        this.setState({
+            homeLink: event.target.value
+        })
+    }
     render(){
-        var text = "something!";
         return (
 
             <div>
                 <p>In a new component!</p>
-                <p>Your name is {this.props.name} and your age is {this.props.age}</p>
-                <p>{text}</p>
-                <div>
-                    <h4>Hobbies</h4>
-                    <ul>
-                        {this.props.user.hobbies.map((hobby , i) => <li key={i}>{hobby}</li>)}
-                    </ul>
-                </div>
+                <p>Your name is {this.props.name} and your age is {this.state.age}</p>
+                <p>Status: {this.state.status} </p>
                 <hr/>
-                {this.props.children}
+                <button onClick={() => this.onMakeOlder()} className={"btn btn-primary"}>Make me older!</button>
+                <hr/>
+                <button onClick={this.props.greet} className={"btn btn-primary"}> Greet </button>
+                <hr/>
+                <input type="text" value={this.state.homeLink}
+                       onChange={(event) => this.onHandleChange(event)}/>
+                <button onClick={this.onChangeLink.bind(this)} className={"btn btn-primary"}>Change Header Link</button>
             </div>
         );
     }
 }
 Home.propTypes = {
-    name: React.PropTypes.string,
-    age: React.PropTypes.number,
-    user: React.PropTypes.object
+    name: PropTypes.string,
+    initialAge: PropTypes.number,
+    greet: PropTypes.func,
+    initialLinkName: PropTypes.string
 };
